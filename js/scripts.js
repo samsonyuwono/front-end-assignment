9e96f2d4fa644d45c54150a672e5915fccb4689b
github username <samson.yuwono@gmail.com>
2019-04-16 13:28:00 -0400

    added js to increase and decrease quantity

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,25 @@ $(".carousel").carousel({
   interval: false
 });
 
-console.log("here");
+let currentQuantity = 1;
+
+const updateQuantity = () => {
+  document.querySelector(".number").innerText =
+    "quantity" + " ( " + currentQuantity + " ) ";
+};
+const subtractButton = document.querySelector(".subtract");
+const plusButton = document.querySelector(".plus");
+subtractButton.addEventListener("click", reduce);
+plusButton.addEventListener("click", increase);
+
+function decrease() {
+  if (currentQuantity > 1) {
+    currentQuantity -= 1;
+  }
+  updateQuantity();
+}
+
+function increase() {
+  currentQuantity += 1;
+  updateQuantity();
+}
