029509cb0f0c1df74a42a140cf49115c650d4541
github username <samson.yuwono@gmail.com>
2019-04-16 14:56:05 -0400

    added js for color checkmark toggling

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,23 @@ $(".carousel").carousel({
   interval: false
 });
 
+const blackColorBox = document.querySelector(".black");
+const beigeColorBox = document.querySelector(".beige");
+const blackCheckMark = document.querySelector(".active-check");
+const beigeCheckMark = document.querySelector(".hidden-check");
+blackColorBox.addEventListener("click", toggleBlackSelection);
+beigeColorBox.addEventListener("click", toggleBeigeSelection);
+
+function toggleBlackSelection() {
+  blackCheckMark.classList.remove("hidden-check");
+  beigeCheckMark.classList.add("hidden-check");
+}
+
+function toggleBeigeSelection() {
+  blackCheckMark.classList.add("hidden-check");
+  beigeCheckMark.classList.remove("hidden-check");
+}
+
 let currentQuantity = 1;
 
 const updateQuantity = () => {
@@ -10,7 +27,7 @@ const updateQuantity = () => {
 };
 const subtractButton = document.querySelector(".subtract");
 const plusButton = document.querySelector(".plus");
-subtractButton.addEventListener("click", reduce);
+subtractButton.addEventListener("click", decrease);
 plusButton.addEventListener("click", increase);
 
 function decrease() {
