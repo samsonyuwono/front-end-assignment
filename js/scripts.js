5575b51da3d1c290bb8a4fa41b2752f659ff96b8
github username <samson.yuwono@gmail.com>
2019-04-17 10:41:03 -0400

    added zoom in/out functionality for images + carousel

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,57 @@ $(".carousel").carousel({
   interval: false
 });
 
+const productImageContainer = document.querySelector(
+  ".product-images-container"
+);
+const productImages = document.querySelectorAll(".product-image");
+productImages.forEach(image => image.addEventListener("click", toggleZoom));
+
+let zoomedIn = false;
+function toggleZoom(event) {
+  const productImageNumber = event.target.dataset.image;
+  const currentZoomClass = document.querySelector(
+    ".zoom-img-" + productImageNumber
+  );
+  if (zoomedIn === false) {
+    zoomedIn = true;
+    productImageContainer.classList.remove("product-image-container");
+    productImageContainer.classList.add("product-zoom-images");
+  } else {
+    zoomedIn = false;
+    productImageContainer.classList.add("product-image-container");
+    productImageContainer.classList.remove("product-zoom-images");
+  }
+  currentZoomClass.scrollIntoView();
+}
+
+const carouselContainer = document.querySelector(".carousel-inner");
+const carouselIndicators = document.querySelector(".carousel-indicators");
+const carouselImages = document.querySelectorAll(".carousel-img");
+carouselImages.forEach(image =>
+  image.addEventListener("click", toggleCarouselZoom)
+);
+
+function toggleCarouselZoom(event) {
+  const carouselImageNumber = event.target.dataset.image;
+  const carouselZoomClass = document.querySelector(
+    ".carousel-zoom-" + carouselImageNumber
+  );
+  if (zoomedIn === false) {
+    zoomedIn = true;
+    carouselIndicators.style.position = "absolute";
+    carouselContainer.classList.remove("carousel-inner");
+    carouselContainer.classList.add("product-zoom-images");
+  } else {
+    //zoomOut needs to be fixed
+    zoomedIn = false;
+    carouselIndicators.style.position = "sticky";
+    carouselContainer.classList.add("carousel-inner");
+    carouselContainer.classList.remove("product-zoom-images");
+  }
+  carouselZoomClass.scrollIntoView();
+}
+
 const blackColorBox = document.querySelector(".black");
 const beigeColorBox = document.querySelector(".beige");
 const blackCheckMark = document.querySelector(".active-check");
